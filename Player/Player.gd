a4605304b94369c03edeb6cea9178f17647df591
BackDrift <aklitouati@hotmail.com>
2026-02-20 09:55:51 +0100

    Cleaned up stair-stepping implementation, made it more modular, and added comments.

--- Player/Player.gd
+++ Player/Player.gd
@@ -47,14 +47,8 @@ var update_camera = false
 var camera_gt_previous : Transform3D
 var camera_gt_current : Transform3D
 
-
-class StepResult:
-	var diff_position: Vector3 = Vector3.ZERO
-	var normal: Vector3 = Vector3.ZERO
-	var is_step_up: bool = false
-
-
 func _ready():
+	#floor_snap_length = 0.0;
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	camera_target_position = camera.global_transform.origin
@@ -67,6 +61,13 @@ func _ready():
 func update_camera_transform():
 	camera_gt_previous = camera_gt_current
 	camera_gt_current = camera_target.global_transform
+
+# Function: Handle mouse mode toggling
+func _toggle_mouse_mode():
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _process(delta: float) -> void:
 	if update_camera:
@@ -97,6 +98,10 @@ func _process(delta: float) -> void:
 		camera.position.y = camera_target_position.y
 
 func _input(event):
+	# Handle ESC input
+	if event.is_action_pressed("ui_cancel"):
+		_toggle_mouse_mode()
+		
 	if event is InputEventMouseMotion:
 		body.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
@@ -124,11 +129,12 @@ func _physics_process(delta):
 		is_in_air = false
 		gravity_direction = Vector3.UP * jump
 
+	direction *= input.length();
 	main_velocity = main_velocity.lerp(direction * speed, acceleration * delta)
 
 	var step_result : StepResult = StepResult.new()
 	
-	is_step = step_check(delta, is_jumping, step_result)
+	is_step = step_check(main_velocity * delta, is_jumping, step_result)
 	
 	if is_step:
 		var is_enabled_stair_stepping: bool = true
@@ -160,9 +166,120 @@ func _physics_process(delta):
 		is_jumping = false
 		is_in_air = true
 
-func step_check(delta: float, is_jumping_: bool, step_result: StepResult):
-	var is_step: bool = false
+class StepResult:
+	var diff_position: Vector3 = Vector3.ZERO
+	var normal: Vector3 = Vector3.ZERO
+	var is_step_up: bool = false
+
+func test_motion(transform3d: Transform3D, motion: Vector3, recovery_as_collision: bool = false) -> PhysicsTestMotionResult3D:
+	var test_params = PhysicsTestMotionParameters3D.new()
+	test_params.from = transform3d
+	test_params.motion = motion
+	test_params.recovery_as_collision = recovery_as_collision;
 	
+	var result = PhysicsTestMotionResult3D.new()
+	var collided = PhysicsServer3D.body_test_motion(self.get_rid(), test_params, result)
+	if collided:
+		return result
+	return null
+
+func step_up(move: Vector3, step_result: StepResult) -> bool:
+	#if gravity is pulling is down we must be fallinga and should not stair step.
+	if gravity_direction.y < 0:
+		return false;
+		
+	for i in range(STEP_CHECK_COUNT):	
+		var step_height: Vector3 = step_height_main - i * step_incremental_check_height;
+		var transform3d: Transform3D = global_transform;
+		
+		#Tests if moving up by step_height collides with anything.
+		#If there’s a collision from above, skip this step height and try the next because we hit a ceiling.	
+		var up_result = test_motion(transform3d, step_height);
+		if(up_result && up_result.get_collision_normal().y < 0):
+			continue;
+		
+		#We are allowed to move up with because there is no ceiling.
+		transform3d.origin += step_height;
+		#After moving up by step_height, test if moving forward will collide.
+		var forward_result = test_motion(transform3d, move);
+		if (forward_result == null):
+			#We are allowed to move forward because there is no wall.
+			transform3d.origin += move;
+			#After moving forward, test if moving down will collide.
+			var down_result = test_motion(transform3d, -step_height);	
+			if (down_result != null):
+				if down_result.get_collision_normal().angle_to(Vector3.UP) <= deg_to_rad(STEP_MAX_SLOPE_DEGREE):
+					step_result.is_step_up = true;
+					step_result.diff_position = -down_result.get_remainder();
+					step_result.normal = down_result.get_collision_normal();
+					return true;
+		else:
+			#We must have collided with a wall.
+			#We add the distance we traveled against the wall.
+			transform3d.origin += forward_result.get_travel();
+			#Then move slightly away from the wall. 
+			var wall_collision_normal: Vector3 = forward_result.get_collision_normal();
+			transform3d.origin += wall_collision_normal * WALL_MARGIN
+			#Then test our remaining movement sliding along the wall
+			var slide_movement = forward_result.get_remainder().slide(wall_collision_normal);
+			var slide_result = test_motion(transform3d, slide_movement);
+			
+			if (slide_result  == null):
+				#If we hit nothing add or sliding movement
+				transform3d.origin += slide_movement
+				#After sliding, test if moving down will collide.
+				var down_result = test_motion(transform3d, -step_height);
+				if (down_result != null):
+					if down_result.get_collision_normal().angle_to(Vector3.UP) <= deg_to_rad(STEP_MAX_SLOPE_DEGREE):
+						step_result.is_step_up = true
+						step_result.diff_position = -down_result.get_remainder();
+						step_result.normal = down_result.get_collision_normal();
+						return true;
+	return false;
+	
+func step_down(move: Vector3, step_result: StepResult):
+	step_result.is_step_up = false
+	var transform3d: Transform3D = global_transform
+
+	var forward_result = test_motion(transform3d, move, true);
+	if (!forward_result):
+		#We are allowed to move forward because there is no wall.
+		transform3d.origin += move
+		#After moving forward, test if moving down will collide.
+		var down_result = test_motion(transform3d, -step_height_main, true);	
+		if (down_result != null):
+			if down_result.get_travel().y < -STEP_DOWN_MARGIN:
+				if down_result.get_collision_normal().angle_to(Vector3.UP) <= deg_to_rad(STEP_MAX_SLOPE_DEGREE):
+					step_result.diff_position = down_result.get_travel()
+					step_result.normal = down_result.get_collision_normal()
+					return true
+
+	elif is_zero_approx(forward_result.get_collision_normal().y):
+		#We must have collided with a wall.
+		#We add the distance we traveled against the wall.
+		transform3d.origin += forward_result.get_travel();
+		#Then move slightly away from the wall. 
+		var wall_collision_normal: Vector3 = forward_result.get_collision_normal()
+		transform3d.origin += wall_collision_normal * WALL_MARGIN
+		#Then test our remaining movement sliding along the wall
+		var slide_movement = forward_result.get_remainder().slide(wall_collision_normal);	
+		var slide_result = test_motion(transform3d, slide_movement, true);
+		if (!slide_result):
+			#If we hit nothing add or sliding movement
+			transform3d.origin += slide_movement
+
+			#After sliding, test if moving down will collide.
+			var down_result = test_motion(transform3d, -step_height_main, true);	
+			if (down_result != null):
+				if down_result.get_travel().y < -STEP_DOWN_MARGIN:
+					if down_result.get_collision_normal().angle_to(Vector3.UP) <= deg_to_rad(STEP_MAX_SLOPE_DEGREE):
+						step_result.diff_position = down_result.get_travel()
+						step_result.normal = down_result.get_collision_normal()
+						return true
+	return false;
+
+	
+func step_check(move: Vector3, is_jumping_: bool, step_result: StepResult):
 	step_height_main = STEP_HEIGHT_DEFAULT
 	step_incremental_check_height = STEP_HEIGHT_DEFAULT / STEP_CHECK_COUNT
 	
@@ -170,115 +287,8 @@ func step_check(delta: float, is_jumping_: bool, step_result: StepResult):
 		step_height_main = STEP_HEIGHT_IN_AIR_DEFAULT
 		step_incremental_check_height = STEP_HEIGHT_IN_AIR_DEFAULT / STEP_CHECK_COUNT
 		
-	if gravity_direction.y >= 0:
-		for i in range(STEP_CHECK_COUNT):
-			var test_motion_result: PhysicsTestMotionResult3D = PhysicsTestMotionResult3D.new()
-			
-			var step_height: Vector3 = step_height_main - i * step_incremental_check_height
-			var transform3d: Transform3D = global_transform
-			var motion: Vector3 = step_height
-			var test_motion_params: PhysicsTestMotionParameters3D = PhysicsTestMotionParameters3D.new()
-			test_motion_params.from = transform3d
-			test_motion_params.motion = motion
-			
-			var is_player_collided: bool = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-
-			if is_player_collided and test_motion_result.get_collision_normal().y < 0:
-				continue
-
-			transform3d.origin += step_height
-			motion = main_velocity * delta
-			test_motion_params.from = transform3d
-			test_motion_params.motion = motion
-			
-			is_player_collided = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-			
-			if not is_player_collided:
-				transform3d.origin += motion
-				motion = -step_height
-				test_motion_params.from = transform3d
-				test_motion_params.motion = motion
-				
-				is_player_collided = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-				
-				if is_player_collided:
-					if test_motion_result.get_collision_normal().angle_to(Vector3.UP) <= deg_to_rad(STEP_MAX_SLOPE_DEGREE):
-						is_step = true
-						step_result.is_step_up = true
-						step_result.diff_position = -test_motion_result.get_remainder()
-						step_result.normal = test_motion_result.get_collision_normal()
-						break
-			else:
-				var wall_collision_normal: Vector3 = test_motion_result.get_collision_normal()
-				transform3d.origin += wall_collision_normal * WALL_MARGIN
-				motion = (main_velocity * delta).slide(wall_collision_normal)
-				test_motion_params.from = transform3d
-				test_motion_params.motion = motion
-				
-				is_player_collided = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-				
-				if not is_player_collided:
-					transform3d.origin += motion
-					motion = -step_height
-					test_motion_params.from = transform3d
-					test_motion_params.motion = motion
-					
-					is_player_collided = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-					
-					if is_player_collided:
-						if test_motion_result.get_collision_normal().angle_to(Vector3.UP) <= deg_to_rad(STEP_MAX_SLOPE_DEGREE):
-							is_step = true
-							step_result.is_step_up = true
-							step_result.diff_position = -test_motion_result.get_remainder()
-							step_result.normal = test_motion_result.get_collision_normal()
-							break
-
-	if not is_jumping_ and not is_step and is_on_floor():
-		step_result.is_step_up = false
-		var test_motion_result: PhysicsTestMotionResult3D = PhysicsTestMotionResult3D.new()
-		var transform3d: Transform3D = global_transform
-		var motion: Vector3 = main_velocity * delta
-		var test_motion_params: PhysicsTestMotionParameters3D = PhysicsTestMotionParameters3D.new()
-		test_motion_params.from = transform3d
-		test_motion_params.motion = motion
-		test_motion_params.recovery_as_collision = true
-
-		var is_player_collided: bool = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-			
-		if not is_player_collided:
-			transform3d.origin += motion
-			motion = -step_height_main
-			test_motion_params.from = transform3d
-			test_motion_params.motion = motion
-			
-			is_player_collided = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-			
-			if is_player_collided and test_motion_result.get_travel().y < -STEP_DOWN_MARGIN:
-				if test_motion_result.get_collision_normal().angle_to(Vector3.UP) <= deg_to_rad(STEP_MAX_SLOPE_DEGREE):
-					is_step = true
-					step_result.diff_position = test_motion_result.get_travel()
-					step_result.normal = test_motion_result.get_collision_normal()
-		elif is_zero_approx(test_motion_result.get_collision_normal().y):
-			var wall_collision_normal: Vector3 = test_motion_result.get_collision_normal()
-			transform3d.origin += wall_collision_normal * WALL_MARGIN
-			motion = (main_velocity * delta).slide(wall_collision_normal)
-			test_motion_params.from = transform3d
-			test_motion_params.motion = motion
-			
-			is_player_collided = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-			
-			if not is_player_collided:
-				transform3d.origin += motion
-				motion = -step_height_main
-				test_motion_params.from = transform3d
-				test_motion_params.motion = motion
-				
-				is_player_collided = PhysicsServer3D.body_test_motion(self.get_rid(), test_motion_params, test_motion_result)
-				
-				if is_player_collided and test_motion_result.get_travel().y < -STEP_DOWN_MARGIN:
-					if test_motion_result.get_collision_normal().angle_to(Vector3.UP) <= deg_to_rad(STEP_MAX_SLOPE_DEGREE):
-						is_step = true
-						step_result.diff_position = test_motion_result.get_travel()
-						step_result.normal = test_motion_result.get_collision_normal()
-
+	var is_step: bool = step_up(move, step_result);
+	if (!is_jumping_ && !is_step && is_on_floor()):
+		is_step = step_down(move, step_result)
+		
 	return is_step
